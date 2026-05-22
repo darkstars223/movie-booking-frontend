7f118f27d0c565e0a9f4b9a6898d45c1134d3fd2
tetopear <vinhyb198@gmail.com>
2026-05-22 23:34:30 +0700

    ddddddd

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://app-fbb420bd-d1bb-4470-b979-da0ba66940ba.cleverapps.io/' || 'http://localhost:5000/api'
+    baseURL: 'https://app-fbb420bd-d1bb-4470-b979-da0ba66940ba.cleverapps.io/api/' || 'http://localhost:5000/api'
 });
 
 export default instance;
